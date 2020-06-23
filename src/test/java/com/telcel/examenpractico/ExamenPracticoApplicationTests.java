e5efa4a10d11426c6cffdfada75c48de5a11cdd2
RickGama <66316588+RickGama@users.noreply.github.com>
2020-06-23 13:59:59 -0500

    Initial commit

---
+++ src/test/java/com/telcel/examenpractico/ExamenPracticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telcel.examenpractico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenPracticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
